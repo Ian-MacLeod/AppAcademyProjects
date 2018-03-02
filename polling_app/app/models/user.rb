bd4dab598592afdcf8776596c01373dd1a5c0016
Ian MacLeod <ian.a.macleod@gmail.com>
2018-03-01 16:27:35 -0800

    Added associations, validations, and seeded database

--- polling_app/app/models/user.rb
+++ polling_app/app/models/user.rb
@@ -9,4 +9,16 @@
 #
 
 class User < ApplicationRecord
+  validates :user_name, presence: true, uniqueness: true
+
+  has_many :authored_polls,
+           class_name: :Poll,
+           primary_key: :id,
+           foreign_key: :author_id
+
+  has_many :responses,
+           class_name: :Response,
+           primary_key: :id,
+           foreign_key: :user_id
+
 end
