f3e8409b02ab24c55a6d1bef34e2a798ebb28157
Ian MacLeod <ian.a.macleod@gmail.com>
2018-03-14 18:13:32 -0700

    Start work on exception middleware

--- W5D3/skeleton/lib/show_exceptions.rb
+++ W5D3/skeleton/lib/show_exceptions.rb
@@ -2,14 +2,25 @@ require 'erb'
 
 class ShowExceptions
   def initialize(app)
+    @app = app
   end
 
   def call(env)
+    begin
+      app.call(env)
+    rescue RuntimeError => e
+      render_exception(e)
+    end
   end
 
   private
 
   def render_exception(e)
+    contents = File.read("lib/templates/rescue.html.erb")
+    template = ERB.new(contents)
+    template.result(binding)
+    res.status = 500
+    res.write(template)
+    res.finish
   end
-
 end
